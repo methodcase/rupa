a52ffd4ea38e7256abdda52d024d46eb88ab9c22
methodcase <methodcase@gmail.com>
2016-12-05 20:39:31 +0800

    add menu

---
+++ components/menu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+/**
+ * Simple Icon Menus demonstrating some of the layouts possible using the `anchorOrigin` and
+ * `targetOrigin` properties.
+ */
+const Menu = () => (
+  <div>
+    <IconMenu
+      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+      anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+      targetOrigin={{horizontal: 'left', vertical: 'top'}}
+    >
+    
+      <MenuItem primaryText="Refresh" />
+      <MenuItem primaryText="Send feedback" />
+      <MenuItem primaryText="Settings" />
+      <MenuItem primaryText="Help" />
+      <MenuItem primaryText="Sign out" />
+    </IconMenu>
+    
+  </div>
+);
+
+export default Menu;
